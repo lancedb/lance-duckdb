873452426e9f842e55cd53f20bfab585c32bb4c0
Xuanwo <github@xuanwo.io>
2025-08-13 21:53:45 +0800

    Save work
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,8 +1,10 @@
-# This file is included by DuckDB's build system to configure the extension
+# This file is included by DuckDB's build system. It specifies which extension to load
 
 # Extension from this repo
 duckdb_extension_load(lance
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
-    INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/include
     LOAD_TESTS
 )
+
+# Any extra extensions that should be built
+# e.g.: duckdb_extension_load(json)
